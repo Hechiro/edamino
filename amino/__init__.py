176fa787dad04ff68047b4515f5ccb1d60d7d378
Ðexteige <exteige@gmail.com>
2021-04-01 17:32:30 +0400

    add amino

---
+++ amino/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'amino'
+__author__ = 'Hechiro'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020-2021 Hechiro'
+__version__ = '1.2.13.1'
+
+
+from .client import Client
+from requests import get
+from json import loads
---
+++ amino/client.py
@@ -0,0 +1,5 @@
+import requests
+
+
+class Client:
+    pass
